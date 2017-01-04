58156af85230625cd4708381966d15274a75cc9d
Liang Hong <lianghongbuaa@gmail.com>
2017-01-04 10:59:17 +0800

    Try to use inputStreamResource to download file. Convert the 'File' to 'Object' to download, but it seems useless. Use IOUtils intead of the method I wrote.

--- src/main/java/spike/emde/file/FileTransfer.java
+++ src/main/java/spike/emde/file/FileTransfer.java
@@ -1,9 +1,16 @@
 package spike.emde.file;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -12,22 +19,35 @@ import java.io.*;
 public class FileTransfer {
 
     private final String targetPath = "src/main/resources/upload/";
-    private final File sourceFile = new File("src/main/resources/download/toDownLoadFile");
+    final String sourceFilePath = "download/firstCard.xlsx";
+    private final File sourceFile = new File("src/main/resources/download/firstCard.xlsx");
 
     public static void main(String[] args) {
-        File file = new File("src/main/resources/download/toDownLoadFile");
+        final String sourceFilePath = "download/firstCard.xlsx";
+        ClassPathResource xlsxFile = new ClassPathResource(sourceFilePath);
+        System.out.println(xlsxFile.getFilename());
     }
 
-    @GetMapping(value = "/download406", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity downloadLocalFile() {
-        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header("Content-Disposition", "attachment; filename=\"" + sourceFile.getName() + "\"").body(sourceFile);
+    @GetMapping(value = "/downloadxlsx", produces = "application/xlsx")
+    public ResponseEntity<InputStreamResource> downloadLocalFile() throws IOException {
+        ClassPathResource xlsxFile = new ClassPathResource(sourceFilePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(xlsxFile.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(xlsxFile.getInputStream()));
     }
 
     @GetMapping(value = "/download")
     public ResponseEntity download() {
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header("Content-Disposition", "attachment; filename=\"" + sourceFile.getName() + "\"").body(sourceFile);
+                .header("Content-Disposition", "attachment; filename=\"" +
+                        sourceFile.getName() + "\"").body((Object) sourceFile);
     }
 
     @GetMapping(value = "/downloadStream406", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
@@ -52,18 +72,10 @@ public class FileTransfer {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This sourceFile already exist.");
         } else {
             FileOutputStream fileOutputStream = new FileOutputStream(file);
-            WriteToFile(inputStream,fileOutputStream);
+            IOUtils.copy(inputStream, fileOutputStream);
             inputStream.close();
             fileOutputStream.close();
             return ResponseEntity.status(HttpStatus.OK).body("File upload successfully");
-        }
-    }
-
-    public void WriteToFile (InputStream inputStream,OutputStream outputStream) throws IOException {
-        int len;
-        byte[] buffer = new byte[4096];
-        while ((len = inputStream.read(buffer, 0, buffer.length)) != -1) {
-            outputStream.write(buffer, 0, len);
         }
     }
 }
