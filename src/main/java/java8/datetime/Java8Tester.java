0d3a8b7be3edee5a781e9e76339551a26521b3ce
Liang Hong <lianghongbuaa@gmail.com>
2017-01-05 13:10:53 +0800

    Reanme package localdateandtime to datetime.

--- src/main/java/java8/datetime/Java8Tester.java
+++ src/main/java/java8/datetime/Java8Tester.java
@@ -1,14 +1,12 @@
-package java8.localdateandtime;
+package java8.datetime;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.LocalDateTime;
-import java.time.Month;
+import java.time.*;
 
 public class Java8Tester {
     public static void main(String args[]){
         Java8Tester java8tester = new Java8Tester();
         java8tester.testLocalDateTime();
+        java8tester.testZonedDateTime();
     }
 
     public void testLocalDateTime(){
@@ -39,5 +37,17 @@ public class Java8Tester {
         //parse a string
         LocalTime date5 = LocalTime.parse("20:15:30");
         System.out.println("date5: " + date5);
+    }
+
+    public void testZonedDateTime(){
+        // Get the current date and time
+        ZonedDateTime date1 = ZonedDateTime.parse("2007-12-03T10:15:30+05:30[Asia/Karachi]");
+        System.out.println("date1: " + date1);
+
+        ZoneId id = ZoneId.of("Europe/Paris");
+        System.out.println("ZoneId: " + id);
+
+        ZoneId currentZone = ZoneId.systemDefault();
+        System.out.println("CurrentZone: " + currentZone);
     }
 }
