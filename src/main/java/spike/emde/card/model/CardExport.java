8cd10a7f01c212ea1ba227e2df9e2759b5286d05
Liang Hong <lianghongbuaa@gmail.com>
2017-01-09 20:48:02 +0800

    Add new CardExport.

---
+++ src/main/java/spike/emde/card/model/CardExport.java
@@ -0,0 +1,84 @@
+package spike.emde.card.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CardExport implements Exportable {
+    private static final List<String> SCHEMA = Arrays.asList(
+            "id", "brief", "content", "assignedTo", "dueDate", "size"
+    );
+
+    private String id;
+    private String brief;
+    private String content;
+    private List<String> assignedTo;
+    private LocalDate dueDate;
+    private String size;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(List<String> assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public List<String> toList() {
+        List<String> arrayList = new ArrayList();
+        arrayList.add(id);
+        arrayList.add(brief);
+        arrayList.add(content);
+        arrayList.add(assignedTo.stream().collect(Collectors.joining(", ")));
+        arrayList.add(dueDate.toString());
+        arrayList.add(size);
+        return arrayList;
+    }
+
+    public static List<String> getSchema() {
+        return SCHEMA;
+    }
+}
