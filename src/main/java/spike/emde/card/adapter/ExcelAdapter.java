4e6d1111538413d3756fd22b7ffd833f3198fbe1
Liang Hong <lianghongbuaa@gmail.com>
2017-01-09 20:41:39 +0800

    Redifne the structure of Adapter.

--- src/main/java/spike/emde/card/adapter/ExcelAdapter.java
+++ src/main/java/spike/emde/card/adapter/ExcelAdapter.java
@@ -1,5 +1,9 @@
 package spike.emde.card.adapter;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.springframework.core.io.ByteArrayResource;
@@ -7,28 +11,35 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import spike.emde.card.model.CardExport;
 import spike.emde.card.model.CardImport;
+import spike.emde.card.model.Exportable;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
-import static spike.emde.utils.ExcelUtils.writeToSheet;
 
 @Component
 public class ExcelAdapter implements FileAdapter {
+
     @Override
-    public Optional<Resource> write(CardExport... cardExport) {
+    public Optional<Resource> write(Exportable... cardExport){ // TODO: rename here
         List<List<String>> content = new ArrayList<>();
-        content.add(CardExport.getScheme());
-        content.addAll(Arrays.stream(cardExport)
-                .map(cardExport1 -> cardExport1.toList())
-                .collect(Collectors.toList()));
-        Resource resource = null;
-        resource = getSXSSFWorkbookResource(content);
-        return Optional.ofNullable(resource);
+        content.add(CardExport.getSchema()); // TODO: annotation
+
+        Arrays.asList(cardExport).stream().map(Exportable::toList).forEach(content::add);
+
+        // Arrays.asList(cardExport).stream().collect(groupBy)
+
+        // card card people people label
+        // group by
+
+        // schema data data data -> sheet 1
+        // schema data data -> sheet 2
+        // schema data -> sheet 3
+        // version -> sheet 4 version 4
+
+        return getWorkbookResource(content);
     }
 
     @Override
@@ -36,15 +47,37 @@ public class ExcelAdapter implements FileAdapter {
         return null;
     }
 
-    public Resource getSXSSFWorkbookResource(List<List<String>> content) {
-        SXSSFWorkbook sheets = new SXSSFWorkbook(100);
-        writeToSheet(sheets.createSheet(), content);
+    private Optional<Resource> getWorkbookResource(List<List<String>> content){
+        Workbook workbook = writeToWorkBook(content);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try {
-            sheets.write(byteArrayOutputStream);
+            workbook.write(byteArrayOutputStream);
+            // Optional.of or Optional.ofNullable
+            return Optional.of(new ByteArrayResource(byteArrayOutputStream.toByteArray()));
         } catch (IOException e) {
-            e.printStackTrace();
+            return Optional.empty();
+            //e.printStackTrace(); // TODO: log
+            // Pretend there is a log.
+        } finally {
+            try {
+                byteArrayOutputStream.close();
+            } catch (IOException ignored) {
+            }
         }
-        return new ByteArrayResource(byteArrayOutputStream.toByteArray());
+    }
+
+    private Workbook writeToWorkBook(List<List<String>> content) {
+        SXSSFWorkbook sheets = new SXSSFWorkbook(100);
+        Sheet sheet = sheets.createSheet();
+        int rowSeq = 0;
+        for(List<String> listString : content) {
+            Row row = sheet.createRow(rowSeq++);
+            int cellSeq = 0;
+            for(String cellString : listString) {
+                Cell cell = row.createCell(cellSeq++);
+                cell.setCellValue(cellString);
+            }
+        }
+        return sheets;
     }
 }
--- src/main/java/spike/emde/card/adapter/FileAdapter.java
+++ src/main/java/spike/emde/card/adapter/FileAdapter.java
@@ -2,14 +2,14 @@ package spike.emde.card.adapter;
 
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-import spike.emde.card.model.CardExport;
 import spike.emde.card.model.CardImport;
+import spike.emde.card.model.Exportable;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public interface FileAdapter {
-    Optional<Resource> write(CardExport... cardExport);
+    Optional<Resource> write(Exportable... cardExport);
     List<CardImport> read(Resource resource);
 }
