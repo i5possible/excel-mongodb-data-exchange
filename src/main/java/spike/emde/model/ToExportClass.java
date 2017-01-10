4599eedca330ed505ebda99b080a3040bbe0c569
Liang Hong <lianghongbuaa@gmail.com>
2017-01-10 17:06:39 +0800

    Add ToExportClass annotation.

---
+++ src/main/java/spike/emde/model/ToExportClass.java
@@ -0,0 +1,10 @@
+package spike.emde.model;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ToExportClass {
+    String exportName() default "";
+}
